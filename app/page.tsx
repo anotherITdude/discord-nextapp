2eff06d543aedc89432a39bd977abfaa66735361
anotherITdude <abhi.baidya@gmail.com>
2023-11-18 14:23:24 +0100

    Fix: test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <p className='text-3xl font-bold text-indigo-500'>
-      Discord clone
+      Discord clone gg
    </p>
   )
 }
